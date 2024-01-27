9ab7afffa709472ed17433e8978da94af6a9d2d8
naveed-rana <rana.naveed812@gmail.com>
2024-01-27 20:26:42 +0500

    class 11

---
+++ learn-python/class11/class11.py
@@ -0,0 +1,153 @@
+# create student app in python, 
+# default 5 students entities should be there in list by default
+# add new student 
+# update any student
+# delete any student
+# show all student's emails
+laptop = {}
+laptop["color"]  = input("Enter color of lapopt")
+
+
+
+laptop = {
+    "company":"dell",
+    "price":234234,
+    "generation":"2nd",
+    "ram":32,
+    "storage":500,
+    "size":15
+}
+laptop["color"] = "black"
+laptop["model"] = "1235",
+laptop["color"] = "red"
+del laptop["color"]
+print("laptop color", laptop["color"])
+
+
+
+# car = {
+#     "color":"red",
+#     "price": 234234,
+#     "engineCC": "1200cc",
+#     "isNew": True,
+#     "anything": None,
+#     "seatHeights":[30,40,50,20],
+     
+# }
+
+# result = car["seatHeights"]
+
+# print("car",result[0])
+
+# student = {
+#     "name": "Ali",
+#     "email": "student@gmail.com",
+#     "rollNo": "1",
+#     "age": 18,
+#     "class": "WMA",
+#     "cnic":234234234,
+    
+# }
+# print("student",student["cnic"])
+
+# message =  {
+#     "text": "Hello, Sir"
+# }
+
+# post = {
+#     "title": "Hello Frields",
+#     "caption": "lrore",
+#     "meda": "url",
+#     "postAt": "10/11/123",
+#     "postTime": "23:234:234",
+#     "comments": [],
+#     "likesCount": 234,
+#     "shareCount": 13,
+# }
+# order = {
+#     "total": 23423,
+#     "isDelivered": True,
+#     "time": "23:324:21",
+#     "type": "dining",
+#     "menu": ["piza", "pasta", "showarma","burger"],
+#     "customer": {
+#         "address":"sdf",
+#         "name": "Naveed",
+#         "paymentMethod": "cash"
+#     }
+# }
+# output = order["customer"]
+# print("total", order["paymentMethod"])
+
+
+
+# order1 = {
+#     "total": 9234,
+#     "isDelivered": True,
+#     "time": "23:324:21",
+#     "type": "dining",
+#     "menu": ["piza", "pasta", "showarma","burger"],
+#     "customer": {
+#         "address":"sdf",
+#         "name": "Naveed",
+#         "paymentMethod": "cash"
+#     }
+# }
+
+# order2 = {
+#     "total": 24,
+#     "isDelivered": True,
+#     "time": "23:324:21",
+#     "type": "dining",
+#     "menu": ["piza", "pasta", "showarma","burger"],
+#     "customer": {
+#         "address":"sdf",
+#         "name": "Naveed",
+#         "paymentMethod": "cash"
+#     }
+# }
+
+# orders = [{
+#     "total": 23423,
+#     "isDelivered": True,
+#     "time": "23:324:21",
+#     "type": "dining",
+#     "menu": ["piza", "pasta", "showarma","burger"],
+#     "customer": {
+#         "address":"sdf",
+#         "name": "Naveed",
+#         "paymentMethod": "cash"
+#     }
+# }, {
+#     "total": 90234,
+#     "isDelivered": True,
+#     "time": "23:324:21",
+#     "type": "dining",
+#     "menu": ["piza", "pasta", "showarma","burger"],
+#     "customer": {
+#         "address":"sdf",
+#         "name": "Naveed",
+#         "paymentMethod": "cash"
+#     }
+# }, {
+#     "total": 234,
+#     "isDelivered": True,
+#     "time": "23:324:21",
+#     "type": "dining",
+#     "menu": ["piza", "pasta", "showarma","burger"],
+#     "customer": {
+#         "address":"sdf",
+#         "name": "Naveed",
+#         "paymentMethod": "cash"
+#     }
+# }]
+
+# carList = [10,20]
+# userName = "Naveed"
+# carColor = "red"
+# carSpeed= 130
+# numberOfDoors = 4
+
+# car = ["carColor","red", 130, 4]
+# nums = [10,20,30,40,50,60]
+
